1d8b706c9566519e9ca594dde3fb8224e847e3a9
Ebmtranceboy <ebmtranceboy@gmail.com>
2017-04-14 14:50:50 +0000

    stmal 1704

--- romberg.c
+++ romberg.c
@@ -34,7 +34,7 @@ romberg(double (*f/* function to integrate */)(double), double /*lower limit*/ a
       }
 
       //Dump ith column of R, R[i,i] is the best estimate so far
-      dump_row(i, Rc);
+//      dump_row(i, Rc);
 
       if(i > 1 && fabs(Rp[i-1]-Rc[i]) < acc){
          return Rc[i-1];
@@ -48,12 +48,43 @@ romberg(double (*f/* function to integrate */)(double), double /*lower limit*/ a
    return Rp[max_steps-1]; //return our best guess
 }
 
-double inverse(double x){ return 1.0/x;}
+#define PI 3.141592653589793
+
+double 
+inverse(double x){ return 1.0/x;}
+
+double 
+gaussian(double x){ return exp(-x*x/2.0)/sqrt(2.0*PI);}
+
+double 
+diff(double (*f)(double), double x, size_t max_steps, double acc){
+  double dx=0.01;
+  double df,df0;
+  for(size_t i=0; i<max_steps;i++){
+     dx/=2.0;
+     df= (f(x+dx)-f(x-dx))/dx/2.0;
+     if(fabs(df-df0)<acc) return df;
+     df0=df;
+  }
+  return df;
+}
+
+double curvilinear_integral(double (*f)(double,double,double), double (*x)(double),double (*y)(double),double (*z)(double), double t0, double t1, size_t max_steps, double acc){
+  
+  speed
+}
 
 int
 main(){
+       
+        printf("\nDerivation:");
+	printf("Expected : %0.18lf        Computed : %0.18lf\n"
+	      ,-0.25,             diff(inverse,2, 100, 1e-8));
 
+        printf("\nIntegration:");
 	printf("Expected : %0.18lf        Computed : %0.18lf\n"
 	      , log(3.14),             romberg(inverse,1.0, 3.14, 100, 1e-8));
+	printf("Expected : %0.18lf        Computed : %0.18lf\n"
+	      , 1.0,             romberg(gaussian,-9999.9, 9999.9, 100, 1e-8));
 	return 0;
 }
